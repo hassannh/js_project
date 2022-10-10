7540b2796b963a1cd90831546e061c44fb95201c
hassannh <hassannouhi91@gmail.com>
2022-10-10 03:24:24 +0200

    first push

---
+++ script.js
@@ -0,0 +1,19 @@
+//alert("hello js");
+var person = {
+    name : 'hassan',
+    note : 16,
+    city : 'agadir',
+}
+console.log(person);
+
+var note = 18;
+
+if(note >= 10){
+
+    alert("congrats")
+}else{
+    alert("sorry you fail")
+}
+
+var x = 38%5
+alert(x);
